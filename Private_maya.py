7dacb9f9fb86e05322234509b2426f8f6f0a011d
ismaya123 <ismayaaryani05@gmail.com>
2022-04-03 17:42:53 +0800

    OPP

---
+++ Private_maya.py
@@ -0,0 +1,13 @@
+class Data():
+    __items = 4 # private
+ 
+    def __init__(self,nama_barang):
+        self.nama = nama_barang
+ 
+    def harga_barang(self,harga_barang):
+       hasil = self.__items * harga_barang
+       return hasil
+ 
+Boneka = Data("Boneka")
+print(Boneka.harga_barang(850))
+ 
